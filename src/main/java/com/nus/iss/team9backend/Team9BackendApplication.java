821123e7c884fce7cf06d4f4995455f60f16b4af
boon96 <boonchungoh@gmail.com>
2024-02-22 16:36:15 +0800

    initial commit

---
+++ src/main/java/com/nus/iss/team9backend/Team9BackendApplication.java
@@ -0,0 +1,13 @@
+package com.nus.iss.team9backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team9BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team9BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nus/iss/team9backend/Team9BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nus.iss.team9backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team9BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
