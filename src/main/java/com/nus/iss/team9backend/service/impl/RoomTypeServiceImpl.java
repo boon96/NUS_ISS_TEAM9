cd66513193513ce58036a4eaf4a4c6a15391f0c1
boon96 <boonchungoh@gmail.com>
2024-02-25 20:51:51 +0800

    Added Comments for RoomType

--- src/main/java/com/nus/iss/team9backend/service/impl/RoomTypeServiceImpl.java
+++ src/main/java/com/nus/iss/team9backend/service/impl/RoomTypeServiceImpl.java
@@ -17,9 +17,9 @@ public class RoomTypeServiceImpl implements RoomTypeService {
 
     @Override
     public RoomTypeDTO createRoomType(RoomTypeDTO roomTypeDTO) {
-        RoomType roomType = RoomTypeMapper.mapToRoomType(roomTypeDTO);
-        RoomType saveRoomType = roomTypeRepository.save(roomType);
-        return RoomTypeMapper.mapToRoomTypeDto(saveRoomType);
+        RoomType roomType = RoomTypeMapper.mapToRoomType(roomTypeDTO);// takes in a DTO from an input form and converts it into an object
+        RoomType saveRoomType = roomTypeRepository.save(roomType);// save into db
+        return RoomTypeMapper.mapToRoomTypeDto(saveRoomType);// return as DTO to be represented as a model to be sent from or to the API client.
 
     }
 }
