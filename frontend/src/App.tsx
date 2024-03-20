6c5adbbc393f329c0cfdbd14f034cd3864f0eb0c
Oh Jun Jie <junjie.oh@ncs.com.sg>
2024-03-20 14:55:21 +0800

    added listing of sample beds

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppRoutes from './routes';
 import HotelHome from './modules/home/login';
 import { HotelBooking } from './modules/hotel/booking';
+import Hotel from './modules/hotel/Hotel';
 import Header from './modules/shared/header/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,6 +16,7 @@ export const App = () => {
         <Routes>
         <Route path="/" element={<HotelHome />} />
         <Route path="/bookings" element={<HotelBooking/> }/>
+        <Route path="/hotel" element={<Hotel/>}/>
           {/* <AppRoutes /> Use AppRoutes directly */}
         </Routes>
       </BrowserRouter>
