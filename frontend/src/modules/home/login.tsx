fa2e690bde9161d0a0b4f012480131012d62d567
Oh Jun Jie <junjie.oh@ncs.com.sg>
2024-03-25 11:03:53 +0800

    updated api call and used ant design

--- frontend/src/modules/home/login.tsx
+++ frontend/src/modules/home/login.tsx
@@ -5,6 +5,7 @@ import { Navigate, useNavigate } from "react-router-dom";
 import { AppDispatch } from "src/config/store";
 import { Storage } from 'react-jhipster';
 // import { useAppDispatch } from 'react-redux';
+import { Form, Input, Button, Card } from 'antd';
 
 export const HotelHome = () => {
 
@@ -12,6 +13,7 @@ export const HotelHome = () => {
     const dispatch = AppDispatch();
     //to navigate to other page
     const navigate = useNavigate();
+    const [form] = Form.useForm();
 
     const [loginForm, setLoginForm] = useState({
         // name: '',
@@ -46,48 +48,83 @@ export const HotelHome = () => {
             console.log("error onclick" + error);
         }
     };
+    const onFinish = async(values) => {
+        console.log('Received values:', values);
+        try{
+            let result = await dispatch(testlogin(values));
+            if(result.payload == null || result.payload == undefined || result.payload == ""){
+                console.log("no data found");
+                return;
+            }else{
+                Storage.session.set("customer", result.payload['data']);
+                navigate('/');
+            }
+        }
+        catch(error){
+            console.log("error onclick" + error);
+        }
+      };
     return (
-        <div>
-            {/* <div className="col-md-6">
-                <label className="form-label" htmlFor="name">
-                    Enter Username:
-                </label>
-                <input
-                type="text"
-                id="name"
-                name="name"
-                className="form-control"
-                onChange={handleChange} />
-            </div> */}
-
-            <div className="col-md-6">
-                <label className="form-label" htmlFor="emailAddress">
-                    Enter Email Address:
-                </label>
-                <input
-                type="text"
-                id="emailAddress"
-                name="emailAddress"
-                className="form-control"
-                onChange={handleChange} />
-            </div>
-            <div className="col-md-6">
-                <label className="form-label" htmlFor="phoneNumber">
-                    Enter Phone:
-                </label>
-                <input
-                type="number"
-                id="phoneNumber"
-                name="phoneNumber"
-                className="form-control"
-                onChange={handleChange} />
-            </div>
-            <label> result of username {loginForm.emailAddress}</label>
+        // <div>
+        //     <div className="col-md-6">
+        //         <label className="form-label" htmlFor="emailAddress">
+        //             Enter Email Address:
+        //         </label>
+        //         <input
+        //         type="text"
+        //         id="emailAddress"
+        //         name="emailAddress"
+        //         className="form-control"
+        //         onChange={handleChange} />
+        //     </div>
+        //     <div className="col-md-6">
+        //         <label className="form-label" htmlFor="phoneNumber">
+        //             Enter Phone:
+        //         </label>
+        //         <input
+        //         type="number"
+        //         id="phoneNumber"
+        //         name="phoneNumber"
+        //         className="form-control"
+        //         onChange={handleChange} />
+        //     </div>
+        //     <label> result of username {loginForm.emailAddress}</label>
 
 
-            <button type="button" onClick={onClick}>Submit Button</button>
-        </div>
-        
+        //     <button type="button" onClick={onClick}>Submit Button</button>
+        // </div>
+        <Card style={{ width: 600, margin: 'auto' }}>
+
+      <Form
+        form={form}
+        onFinish={onFinish}
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 18 }}
+      >
+        <Form.Item
+          label="Enter Email Address:"
+          name="emailAddress"
+          rules={[{ required: true, message: 'Please enter your email address!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Enter Phone:"
+          name="phoneNumber"
+          rules={[{ required: true, message: 'Please enter your phone number!' }]}
+        >
+          <Input type="number" />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 6, span: 18 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+
+    </Card>
     )
 }
 export default HotelHome;
