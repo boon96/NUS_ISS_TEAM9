fc9e4cffe5c5e76bdda054a7fbf7ab94e2a3d79e
Oh Jun Jie <ojlasiol@gmail.com>
2024-03-26 19:07:47 +0800

    added mockdata to test

--- frontend/src/modules/hotel/searchedItem.tsx
+++ frontend/src/modules/hotel/searchedItem.tsx
@@ -5,6 +5,7 @@ import { AppDispatch } from "src/config/store";
 import axios from "axios";
 import ConfirmationModal from "../shared/UI/reservationPopup";
 import { DatePicker, InputNumber, Button, Form } from 'antd';
+import { mockData } from "src/resources/mockdata";
 
 const SearchItem = (props: any) => {
   //to call api
@@ -36,60 +37,110 @@ const SearchItem = (props: any) => {
   // },[]);
   //retrieve details of searched item
 
+  console.log(mockData)
+
   const onClickFunction = () => {
     navigate('/hotel/confirmation')
   }
   return (
+    // <div className="searchItem">
+    //   <img
+    //     //   src="../../resources/2bed.png"
+    //     src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
+    //     alt=""
+    //     className="siImg"
+    //   />
+    //   <div className="siDesc">
+    //     <h1 className="siTitle">Tower Street Apartments</h1>
+    //     {/* roomname */}
+    //     <span className="siDistance">500m from center</span>
+    //     <span className="siTaxiOp">Free airport taxi</span>
+    //     <span className="siSubtitle">
+    //       Studio Apartment with Air conditioning
+    //     </span>
+    //     <span className="siFeatures">
+    //       Entire studio • 1 bathroom • 21m² 1 full bed
+    //     </span>
+    //     {/* roomDescription */}
+    //     <span className="siCancelOp">Free cancellation </span>
+    //     <span className="siCancelOpSubtitle">
+    //       You can cancel later, so lock in this great price today!
+    //     </span>
+    //   </div>
+    //   <div className="siDetails">
+    //     <div className="siDetailTexts">
+    //       <span className="siPrice">$112</span>
+    //       {/* price */}
+    //       <span className="siTaxOp">Includes taxes and fees</span>
+    //       {props.userLoggedIn ? (
+    //         <div>
+    //           {/* <button className="siCheckButton" onClick={props.handleReservation}>Book Now</button> */}
+    //           <Button type="primary" onClick={() => setIsConfirmationVisible(true)}>
+    //             Submit Reservation
+    //           </Button>
+    //           <ConfirmationModal
+    //             isVisible={isConfirmationVisible}
+    //             onConfirm={props.handleReservation}
+    //             onCancel={() => setIsConfirmationVisible(false)}
+    //             message="Are you sure you want to submit the reservation?"
+    //           />
+    //         </div>
+    //       ) :
+    //         (
+    //           <button className="siCheckButton" onClick={props.handleReservation}>See availability</button>
+    //         )
+    //       }
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="searchItemContainer">
     <div className="searchItem">
-      <img
-        //   src="../../resources/2bed.png"
-        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
-        alt=""
-        className="siImg"
-      />
-      <div className="siDesc">
-        <h1 className="siTitle">Tower Street Apartments</h1>
-        {/* roomname */}
-        <span className="siDistance">500m from center</span>
-        <span className="siTaxiOp">Free airport taxi</span>
-        <span className="siSubtitle">
-          Studio Apartment with Air conditioning
-        </span>
-        <span className="siFeatures">
-          Entire studio • 1 bathroom • 21m² 1 full bed
-        </span>
-        {/* roomDescription */}
-        <span className="siCancelOp">Free cancellation </span>
-        <span className="siCancelOpSubtitle">
-          You can cancel later, so lock in this great price today!
-        </span>
-      </div>
-      <div className="siDetails">
-        <div className="siDetailTexts">
-          <span className="siPrice">$112</span>
-          {/* price */}
-          <span className="siTaxOp">Includes taxes and fees</span>
-          {props.userLoggedIn ? (
-            <div>
-              {/* <button className="siCheckButton" onClick={props.handleReservation}>Book Now</button> */}
-              <Button type="primary" onClick={() => setIsConfirmationVisible(true)}>
-                Submit Reservation
-              </Button>
-              <ConfirmationModal
-                isVisible={isConfirmationVisible}
-                onConfirm={props.handleReservation}
-                onCancel={() => setIsConfirmationVisible(false)}
-                message="Are you sure you want to submit the reservation?"
-              />
+      {/* Render search item details */}
+      {mockData.map((item) => (
+        <div key={item.roomId} className="searchItemContent">
+          {/* Render room image */}
+          <img
+            src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
+            alt=""
+            className="siImg"
+          />
+          {/* Render room details */}
+          <div className="siDesc">
+            <h1 className="siTitle">{item.roomTypeId.name}</h1>
+            <span className="siDistance">500m from center</span>
+            <span className="siTaxiOp">Free airport taxi</span>
+            <span className="siSubtitle">{item.status}</span>
+            <span className="siFeatures">{item.roomTypeId.description}</span>
+            <span className="siCancelOp">Free cancellation</span>
+            <span className="siCancelOpSubtitle">You can cancel later, so lock in this great price today!</span>
+          </div>
+          {/* Render room price and reservation button */}
+          <div className="siDetails">
+            <div className="siDetailTexts">
+              <span className="siPrice">{item.roomTypeId.price}</span>
+              <span className="siTaxOp">Includes taxes and fees</span>
+              {/* Render different buttons based on user login status */}
+              {props.userLoggedIn ? (
+                <div>
+                  <Button type="primary" onClick={() => setIsConfirmationVisible(true)}>
+                    Submit Reservation
+                  </Button>
+                  <ConfirmationModal
+                    isVisible={isConfirmationVisible}
+                    onConfirm={props.handleReservation}
+                    onCancel={() => setIsConfirmationVisible(false)}
+                    message="Are you sure you want to submit the reservation?"
+                  />
+                </div>
+              ) : (
+                <button className="siCheckButton" onClick={onClickFunction}>See availability</button>
+              )}
             </div>
-          ) :
-            (
-              <button className="siCheckButton" onClick={props.handleReservation}>See availability</button>
-            )
-          }
+          </div>
         </div>
-      </div>
+      ))}
     </div>
+     </div>
   );
 };
 
---
+++ frontend/src/resources/mockdata.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+// Mock data array
+export const mockData = [
+    {
+        "roomId": 2,
+        "location": "LEVEL 5",
+        "status": "Available",
+        "roomTypeId": {
+            "id": 2,
+            "name": "Deluxe",
+            "description": "ALOT TOILET",
+            "price": 100.0
+        }
+    },
+    {
+        "roomId": 3,
+        "location": "LEVEL 7",
+        "status": "Available",
+        "roomTypeId": {
+            "id": 3,
+            "name": "Suite",
+            "description": "LUXURY SUITE",
+            "price": 200.0
+        }
+    },
+    {
+        "roomId": 4,
+        "location": "LEVEL 3",
+        "status": "Booked",
+        "roomTypeId": {
+            "id": 1,
+            "name": "Standard",
+            "description": "BASIC ROOM",
+            "price": 80.0
+        }
+    },
+    {
+        "roomId": 5,
+        "location": "LEVEL 2",
+        "status": "Available",
+        "roomTypeId": {
+            "id": 4,
+            "name": "Superior",
+            "description": "SPACIOUS ROOM",
+            "price": 150.0
+        }
+    },
+    {
+        "roomId": 6,
+        "location": "LEVEL 4",
+        "status": "Available",
+        "roomTypeId": {
+            "id": 5,
+            "name": "Penthouse",
+            "description": "LUXURY PENTHOUSE",
+            "price": 300.0
+        }
+    },
+    {
+        "roomId": 7,
+        "location": "LEVEL 6",
+        "status": "Booked",
+        "roomTypeId": {
+            "id": 6,
+            "name": "Executive Suite",
+            "description": "EXECUTIVE LIVING",
+            "price": 250.0
+        }
+    },
+    {
+        "roomId": 8,
+        "location": "LEVEL 8",
+        "status": "Available",
+        "roomTypeId": {
+            "id": 7,
+            "name": "Family Room",
+            "description": "SPACIOUS FAMILY ROOM",
+            "price": 180.0
+        }
+    },
+    {
+        "roomId": 9,
+        "location": "LEVEL 9",
+        "status": "Available",
+        "roomTypeId": {
+            "id": 8,
+            "name": "Ocean View",
+            "description": "ROOM WITH OCEAN VIEW",
+            "price": 220.0
+        }
+    },
+    {
+        "roomId": 10,
+        "location": "LEVEL 10",
+        "status": "Booked",
+        "roomTypeId": {
+            "id": 9,
+            "name": "Studio",
+            "description": "STUDIO ROOM",
+            "price": 120.0
+        }
+    }
+]
+
+
