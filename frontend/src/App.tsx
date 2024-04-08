353a99f3a2fdb37d371e6a8e86a923aab4907e37
Oh Jun Jie <ojlasiol@gmail.com>
2024-04-08 11:38:59 +0800

    fixed naming error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import AppRoutes from './routes';
 import HotelHome from './modules/home/login';
 import { HotelBooking } from './modules/hotel/booking';
-import Hotel from './modules/hotel/Hotel';
+import Hotel from './modules/hotel/hotel';
 import Header from './modules/shared/header/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ContactUsPage from './modules/home/contactUs';
@@ -16,7 +16,6 @@ export const App = () => {
   const baseHref = document.querySelector('base')?.getAttribute('href')?.replace(/\/$/,'');
   return (
     <div className="App">
-      {/* <h1>Hotel Name</h1> */}
       <Header/>
       <BrowserRouter basename={baseHref}>
         <Routes>
